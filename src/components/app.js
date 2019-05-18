d8d6111d9701f9413db87654079a325440ee66c6
riveran <riveranqq@gmail.com>
2019-05-18 17:47:38 +0300

    added support react-scss

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './../css/app.scss'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="main-wrapper">
+        Ready to work
+      </div>
+    )
+  }
+}
